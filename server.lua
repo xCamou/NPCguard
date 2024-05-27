1aba8adfbfa29eec1dc14df40c550d67dc69ed00
Resesetti <88510776+Resesetti@users.noreply.github.com>
2024-05-27 17:09:09 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,12 +1,7 @@
-ESX = exports['es_extended']:getSharedObject()
-
 RegisterCommand('spawnnpc', function(source, args, rawCommand)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local npcType = args[1]
-
-    if npcType and Config.NPCTypes[npcType] then
+    if not args then TriggerClientEvent('esx:showNotification', source, 'Bitte gebe einen gültigen NPC-Typ an.') return end
+    local npcType = tostring(args[1])
+    if Config.NPCTypes[npcType] then
         TriggerClientEvent('spawnFriendlyNPC', source, npcType)
-    else
-        TriggerClientEvent('esx:showNotification', source, 'Bitte gebe einen gültigen NPC-Typ an.')
     end
 end, false)
